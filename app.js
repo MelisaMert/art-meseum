ee4045d72e586d885bdcd95ab6573f42e7f129a7
MelisaMert <melisamert024@gmail.com>
2021-06-13 22:36:50 +0300

    Created Project Folder & app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const appRouter = require('./src/routers/app-router');
+
+app.use(express.json());
+
+app.use('/', appRouter)
+
+
+app.listen(3000,() => {
+    console.log('Server running in 3000 port');
+})
+
